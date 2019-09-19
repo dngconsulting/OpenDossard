89561fef122703acd63bb30d3f3ef05159e38756
Sami <sami.jaber@gmail.com>
2019-09-19 18:52:16 +0200

    Datatable integration

--- src/public/react/dossard-react/src/pages/Coureurs.tsx
+++ src/public/react/dossard-react/src/pages/Coureurs.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
 
-import {Theme, Typography, withStyles} from '@material-ui/core';
+import {Theme, withStyles} from '@material-ui/core';
+import MaterialTable from 'material-table';
 
 interface ICoureursProps {
     items: any[];
     classes: any;
 }
-
+const data = () => {
+    let mytable = []
+    for (let i = 0; i < 1000; i++) {
+        mytable.push({licenceNumber : i, nom : "Nom"+i , prenom : "Prénom"+i, sexe : 'M', dept : '31', age : i%100, catea : 'SENIOR', catev : '4'})
+    }
+    return mytable
+}
 class CoureursPage extends React.Component<ICoureursProps, {}> {
 
     public render(): JSX.Element {
         return (
-            <Typography noWrap={false}>Coureur</Typography>
+            <MaterialTable
+                title="Annuaire des coureurs"
+                columns={[
+                    { title: "Numéro licence", field: "licenceNumber" },
+                    { title: "Nom", field: "nom" },
+                    { title: "Prénom", field: "prenom" },
+                    { title: "Sexe", field: "sexe" },
+                    { title: "Dept", field: "dept" },
+                    { title: "Age", field: "age" },
+                    { title: "CatéA", field: "catea" },
+                    { title: "CatéV", field: "catev" },
+                ]}
+                data={data()}
+                options={{
+                    filtering: true
+                }}
+            />
         );
     }
 }
