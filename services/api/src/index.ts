ea025a0463778a820b2998a7dc432e08cf033995
Sami <sami.jaber@gmail.com>
2019-09-23 17:45:39 +0200

    Fix pascal casing for columns in postgres & nodemon with W10

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,9 +1,6 @@
-import {$log} from "ts-log-debug";
-import {Server} from "./Server";
-import "reflect-metadata";
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {Licence} from "./entity/Licence";
+import {$log} from 'ts-log-debug';
+import {Server} from './Server';
+import 'reflect-metadata';
 
 $log.debug("Start server...");
 new Server().start().catch((er) => {
