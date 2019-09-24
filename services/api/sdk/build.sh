c7fe85f94ad8dc978e470718d3e159f38fe85bc0
Sami <sami.jaber@gmail.com>
2019-09-24 16:12:58 +0200

    Implements Swagger CodeGen with Typescript + fetch

---
+++ services/api/sdk/build.sh
@@ -0,0 +1,23 @@
+ echo "Generation du SDK ..."
+ # CONFIG OPTIONS
+ #        sortParamsByRequiredFlag
+ #            Sort method arguments to place required parameters before optional parameters. (Default: true)
+ #        ensureUniqueParams
+ #            Whether to ensure parameter names are unique in an operation (rename parameters that are not). (Default: true)
+ #        allowUnicodeIdentifiers
+ #            boolean, toggles whether unicode identifiers are allowed in names or not, default is false (Default: false)
+ #        modelPropertyNaming
+ #            Naming convention for the property: 'camelCase', 'PascalCase', 'snake_case' and 'original', which keeps the original name (Default: camelCase)
+ #        supportsES6
+ #            Generate code that conforms to ES6. (Default: false)
+ #        npmName
+ #            The name under which you want to publish generated npm package
+ #        npmVersion
+ #            The version of your npm package
+ #        npmRepository
+ #            Use this property to set an url your private npmRepo in the package.json
+ #        snapshot
+ #            When setting this property to true the version will be suffixed with -SNAPSHOT.yyyyMMddHHmm (Default: false)
+ #        withInterfaces
+ #            Setting this property to true will generate interfaces next to the default class implementations. (Default: false)
+ java -jar swagger-codegen-cli-2.4.8.jar generate -i http://localhost:8080/api-docs/swagger.json -l typescript-fetch -o ../src/sdk
