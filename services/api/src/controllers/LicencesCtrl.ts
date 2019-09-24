488e19526c959d18d724568320cd591e18d2b7d5
gparamelle <gparamelle@palo-it.com>
2019-09-24 16:30:24 +0200

    tslint

--- services/api/src/controllers/LicencesCtrl.ts
+++ services/api/src/controllers/LicencesCtrl.ts
@@ -15,42 +15,42 @@ import {Course} from '../entity/Course';
  * In this case, EventsCtrl is a dependency of CalendarsCtrl.
  * All routes of EventsCtrl will be mounted on the `/calendars` path.
  */
-@Controller("/licences")
+@Controller('/licences')
 export class LicencesCtrl {
-    private licences = require("../../resources/licences.json")
-    @Get("/:id")
-    async get(@Required() @PathParams("id") id: string): Promise<Licence> {
-        throw new NotFound("Not Implemented Yet");
+    private licences = require('../../resources/licences.json');
+    @Get('/:id')
+    public async get(@Required() @PathParams('id') id: string): Promise<Licence> {
+        throw new NotFound('Not Implemented Yet');
     }
 
-    @Get("/")
-    async getAllLicences(): Promise<Licence[]> {
+    @Get('/')
+    public async getAllLicences(): Promise<Licence[]> {
         try {
             const connection = await createConnection();
             const license = await connection.getRepository(Licence).find();
             return license;
         } catch (error) {
-            console.log(error)
+            console.log(error);
             return null;
         }
     }
 
-    @Put("/")
-    async save(@BodyParams("licenceNumber")licenceId: string,
-               @BodyParams("nom") nom: string,
-               @BodyParams("prenom") prenom: string,
-               @BodyParams("genre") sexe: string): Promise<Licence> {
+    @Put('/')
+    public async save(@BodyParams('licenceNumber')licenceId: string,
+               @BodyParams('nom') nom: string,
+               @BodyParams('prenom') prenom: string,
+               @BodyParams('genre') sexe: string): Promise<Licence> {
         const licence = new Licence();
-        createConnection().then(async connection => {
-            console.log("Inserting a new user into the database...");
-            licence.nom = "RUBY";
-            licence.prenom = "Jérome";
-            licence.genre = "H";
+        createConnection().then(async (connection) => {
+            console.log('Inserting a new user into the database...');
+            licence.nom = 'RUBY';
+            licence.prenom = 'Jérome';
+            licence.genre = 'H';
             await connection.manager.save(licence);
-            console.log("Saved a new licence with id: " + licence.id);
+            console.log('Saved a new licence with id: ' + licence.id);
 
-        }).catch(error => console.log(error));
+        }).catch((error) => console.log(error));
 
-      return licence
+        return licence;
     }
 }
--- services/api/src/controllers/calendars/CalendarsCtrl.ts
+++ services/api/src/controllers/calendars/CalendarsCtrl.ts
@@ -8,12 +8,12 @@ import {
   Post,
   Put,
   Required,
-  Status
-} from "@tsed/common";
-import {NotFound} from "ts-httpexceptions";
-import {Calendar} from "../../interfaces/Calendar";
-import {CalendarsService} from "../../services/calendars/CalendarsService";
-import {EventsCtrl} from "../events/EventsCtrl";
+  Status,
+} from '@tsed/common';
+import {NotFound} from 'ts-httpexceptions';
+import {Calendar} from '../../interfaces/Calendar';
+import {CalendarsService} from '../../services/calendars/CalendarsService';
+import {EventsCtrl} from '../events/EventsCtrl';
 
 /**
  * Add @Controller annotation to declare your class as Router controller.
@@ -23,15 +23,15 @@ import {EventsCtrl} from "../events/EventsCtrl";
  * In this case, EventsCtrl is a dependency of CalendarsCtrl.
  * All routes of EventsCtrl will be mounted on the `/calendars` path.
  */
-@Controller("/calendars", EventsCtrl)
+@Controller('/calendars', EventsCtrl)
 export class CalendarsCtrl {
 
   constructor(private calendarsService: CalendarsService) {
 
   }
 
-  @Get("/:id")
-  async get(@Required() @PathParams("id") id: string): Promise<Calendar> {
+  @Get('/:id')
+  public async get(@Required() @PathParams('id') id: string): Promise<Calendar> {
 
     const calendar = await this.calendarsService.find(id);
 
@@ -39,11 +39,11 @@ export class CalendarsCtrl {
       return calendar;
     }
 
-    throw new NotFound("Calendar not found");
+    throw new NotFound('Calendar not found');
   }
 
-  @Put("/")
-  save(@BodyParams("name") name: string) {
+  @Put('/')
+  public save(@BodyParams('name') name: string) {
     return this.calendarsService.create(name);
   }
 
@@ -53,9 +53,9 @@ export class CalendarsCtrl {
    * @param name
    * @returns {Promise<Calendar>}
    */
-  @Post("/:id")
-  async update(@PathParams("id") @Required() id: string,
-               @BodyParams("name") @Required() name: string): Promise<Calendar> {
+  @Post('/:id')
+  public async update(@PathParams('id') @Required() id: string,
+               @BodyParams('name') @Required() name: string): Promise<Calendar> {
     return this.calendarsService.update({id, name});
   }
 
@@ -64,16 +64,16 @@ export class CalendarsCtrl {
    * @param id
    * @returns {{id: string, name: string}}
    */
-  @Delete("/")
+  @Delete('/')
   @Authenticated()
   @Status(204)
-  async remove(@BodyParams("id") @Required() id: string): Promise<void> {
+  public async remove(@BodyParams('id') @Required() id: string): Promise<void> {
     this.calendarsService.remove(id);
   }
 
-  @Get("/")
+  @Get('/')
   @Authenticated()
-  async getAllCalendars(): Promise<Calendar[]> {
+  public async getAllCalendars(): Promise<Calendar[]> {
     return this.calendarsService.query();
   }
 }
