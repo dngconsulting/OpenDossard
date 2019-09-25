5fe2bcd0862f963931844eac504f1b33fe6e551f
gparamelle <gparamelle@palo-it.com>
2019-09-25 12:33:07 +0200

    Retry connection setup on startup

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -2,11 +2,23 @@ import {$log} from 'ts-log-debug';
 import {Server} from './Server';
 import 'reflect-metadata';
 import {createConnection} from 'typeorm';
+import {retry} from './util/retry';
+
+/**
+ * Setup connection with a retry (if database container starts after self container)
+ */
+const setupConnection = async () => {
+  await retry({
+    remaining: 10, delay: 2000, callback: async () => {
+      $log.info('Start createConnection...');
+      await createConnection();
+    },
+  }) ;
+}
 
 const init = async () => {
   try {
-    $log.info('Start createConnection...');
-    await createConnection();
+    await setupConnection();
     $log.info('Start server...');
     await new Server().start();
   } catch (e) {
