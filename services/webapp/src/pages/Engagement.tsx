d447ba46569b398afca09a91aa92585f010a0250
gparamelle <gparamelle@palo-it.com>
2019-10-01 11:56:14 +0200

    Engagement : creation form

--- services/webapp/src/pages/Engagement.tsx
+++ services/webapp/src/pages/Engagement.tsx
@@ -1,24 +1,20 @@
 import * as React from 'react';
 import {useEffect, useState} from 'react';
 
-import {Theme, withStyles} from '@material-ui/core';
+import {createStyles, Theme} from '@material-ui/core';
 import MaterialTable, {Column} from "material-table";
 
 import {apiRaces} from "../util/api";
-import {RaceRow} from "../sdk";
+import {RaceCreate, RaceRow} from "../sdk";
+import Card from "@material-ui/core/Card";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
-
-const styles = (theme : Theme) => ({
-
-});
-
-const create = async (newData: RaceRow, competitionId: number) => {
-    await apiRaces.create({
-        riderNumber: newData.riderNumber,
-        raceCode: newData.raceCode,
-        licenceNumber: newData.licenceNumber,
-        competitionId
-    });
+const create = async (newRace: RaceCreate) => {
+    await apiRaces.create(newRace);
 }
 
 const update = async (newData: RaceRow) => {
@@ -59,38 +55,92 @@ const EngagementPage = ({match}: {match: any}) => {
         fetchData()
     }, ['loading'])
 
-
-
-    return <MaterialTable
-        title={`Engagement ${competitionId}`}
-        columns={COLUMNS}
-        data={races}
-        options={{
-            filtering: true,
-            actionsColumnIndex: -1,
-            pageSize: 500,
-            pageSizeOptions: [],
-            grouping: true,
-        }}
-        editable={{
-            onRowAdd: async (newData) => {
-                await create(newData, competitionId)
-                fetchData();
-            },
-            onRowUpdate: async (newData, oldData) => {
-                await update(newData)
-                fetchData()
-            }
-        }}
-        localization={{
-            grouping: {
-                groupedBy: 'Regroupement par :',
-                placeholder: 'Glisser ici la colonne a regrouper'
-            },
-            body: {}
-        }}
-    />
+    return <div>
+        <CreationForm {...{competitionId, fetchData}}/>
+        <MaterialTable
+            title={`Engagement ${competitionId}`}
+            columns={COLUMNS}
+            data={races}
+            options={{
+                filtering: true,
+                actionsColumnIndex: -1,
+                pageSize: 500,
+                pageSizeOptions: [],
+                grouping: true,
+            }}
+            editable={{
+                onRowUpdate: async (newData, oldData) => {
+                    await update(newData)
+                    fetchData()
+                }
+            }}
+            localization={{
+                grouping: {
+                    groupedBy: 'Regroupement par :',
+                    placeholder: 'Glisser ici la colonne a regrouper'
+                },
+                body: {}
+            }}
+        />
+      </div>
 
 }
 
-export default withStyles(styles as any)(EngagementPage);
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        field: {
+            marginLeft: 10,
+            marginRight: 10
+        },
+    }),
+);
+
+const CreationForm = ({competitionId, fetchData} : { competitionId: number, fetchData: () => {}}) => {
+
+    const [newRace, setValues] = useState<RaceCreate>({
+        competitionId
+    });
+
+    const classes = useStyles({});
+
+    return <Card style={{margin: 20, padding: 20}}>
+        <Typography variant="h6" gutterBottom={true}>
+            Nouveau coureur :
+        </Typography>
+        <Grid container={true} spacing={3} alignItems={"baseline"}>
+            <TextField
+                label="Numéro de licence"
+                value={newRace.licenceNumber}
+                className={classes.field}
+                onChange={e => setValues({...newRace, licenceNumber: e.target.value})}
+                margin="normal"
+            />
+            <TextField
+                label="Numéro de dossard"
+                value={newRace.riderNumber}
+                className={classes.field}
+                onChange={e => setValues({...newRace, riderNumber: parseInt(e.target.value)})}
+                margin="normal"
+            />
+            <TextField
+                label="Course"
+                value={newRace.raceCode}
+                className={classes.field}
+                onChange={e => setValues({...newRace, raceCode: e.target.value})}
+                margin="normal"
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={ async () => {
+                    await create(newRace)
+                    await fetchData()
+                }}
+            >
+                OK
+            </Button>
+        </Grid>
+    </Card>
+}
+
+export default EngagementPage;
