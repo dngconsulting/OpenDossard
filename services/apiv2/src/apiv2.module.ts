0fa88c48a4a6a609730f744360f9705f0d6848f7
Sami <sami.jaber@gmail.com>
2019-10-02 11:14:17 +0200

    Request Log interceptors

--- services/apiv2/src/apiv2.module.ts
+++ services/apiv2/src/apiv2.module.ts
@@ -1,4 +1,4 @@
-import {Module} from '@nestjs/common';
+import {Module, Scope} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {Licence} from './entity/Licence';
 import {Competition} from './entity/Competition';
@@ -12,10 +12,22 @@ import {LocalStrategy} from './services/local.strategy';
 import {PassportModule} from '@nestjs/passport';
 import {PassportCtrl} from './controllers/PassportCtrl';
 import {RacesCtrl} from './controllers/RaceCtrl';
+import {FactoryProvider} from '@nestjs/common/interfaces';
+import {APP_INTERCEPTOR} from '@nestjs/core';
+import {RlogInterceptor} from './interceptors/rlog.interceptor';
+
+const RLog: FactoryProvider = {
+    provide: APP_INTERCEPTOR,
+    scope: Scope.REQUEST,
+    inject: [AppService],
+    useFactory: (appService: AppService) => {
+        return new RlogInterceptor('foo');
+    },
+};
 
 @Module({
     imports: [TypeOrmModule.forFeature([Licence, Club, Competition, Race]), PassportModule],
-    providers: [AppService, UsersService, AuthService, LocalStrategy],
+    providers: [AppService, UsersService, AuthService, LocalStrategy, RLog],
     exports: [UsersService],
     controllers: [LicencesCtrl, PassportCtrl, RacesCtrl],
 })
---
+++ services/apiv2/src/interceptors/rlog.interceptor.ts
@@ -0,0 +1,25 @@
+import {CallHandler, ExecutionContext, Logger, NestInterceptor} from '@nestjs/common';
+import {Observable} from 'rxjs';
+
+export class RlogInterceptor implements NestInterceptor {
+    constructor(
+        private readonly value: any,
+    ) {
+    }
+
+    /**
+     * Cet intercepteur est mis à titre indicatif, n'hésitez pas à le modifier
+     * pour y rajouter des infos supplémentaires
+     * @param context
+     * @param next
+     */
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        Logger.log('--------------------------------------------------------');
+        Logger.log(context.switchToHttp().getRequest().method + ' ' +
+            context.switchToHttp().getRequest().url);
+        Logger.log('Request Body ' + JSON.stringify(context.switchToHttp().getRequest().body));
+        Logger.log('Request Headers ' + JSON.stringify(context.switchToHttp().getRequest().headers));
+        Logger.log('Response ' + JSON.stringify(context.switchToHttp().getResponse().statusCode));
+        return next.handle();
+    }
+}
