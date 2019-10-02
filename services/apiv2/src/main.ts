159a653bd36d8cd5caf3ae2c54da63a3bc74aefb
Sami <sami.jaber@gmail.com>
2019-10-02 10:42:04 +0200

    Startup message in case of hot reload & logger add

--- services/apiv2/src/main.ts
+++ services/apiv2/src/main.ts
@@ -1,18 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import {Apiv2Module} from './apiv2.module';
+import {Logger} from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-      .setTitle('Click And Dossard API V2')
-      .setDescription('Documentation de l\'API Click And Dossard API description')
-      .setVersion('2.0')
-      .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+    const app = await NestFactory.create(AppModule, {logger: ['error', 'warn', 'debug']});
+    const options = new DocumentBuilder()
+        .setTitle('Click And Dossard API V2')
+        .setDescription('Documentation de l\'API Click And Dossard API description')
+        .setVersion('2.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(9090);
+    Logger.debug(`┌────────────────────────────────────────────────────────────┐`);
+    Logger.debug(`│    Starting Click & Dossard with NestJS Framework          │`);
+    Logger.debug(`└────────────────────────────────────────────────────────────┘`);
+
+    await app.listen(9090);
 }
+
 bootstrap();
