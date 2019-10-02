8663eb36f5033e3f328940e8406a2f9098fd4f74
Sami <sami.jaber@gmail.com>
2019-10-02 10:27:36 +0200

    Typo fix

--- services/apiv2/src/services/local.strategy.ts
+++ services/apiv2/src/services/local.strategy.ts
@@ -13,7 +13,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(username: string, password: string): Promise<any> {
-        console.log('Validation du username ' + username + ' pass=' + password);
         const user = await this.authService.validateUser(username, password);
         if (!user) {
             throw new UnauthorizedException();
