9a640a11f4f52aa5344bff80a0a6382f815c6a79
gparamelle <gparamelle@palo-it.com>
2019-10-03 18:28:44 +0200

    RaceTabs : design

--- services/webapp/src/components/RaceTabs.tsx
+++ services/webapp/src/components/RaceTabs.tsx
@@ -1,6 +1,7 @@
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import * as React from "react";
+import {makeStyles} from "@material-ui/core";
 
 export interface IRaceStat {[code:string] : number}
 
@@ -10,18 +11,51 @@ interface IRaceTabs {
     onChange: (value:string) => void
 }
 
+const useStyle = makeStyles(theme => ({
+    tab: {
+        display: 'inline-block',
+    },
+    button: {
+        background: theme.palette.common.white,
+        paddingLeft: 50, paddingRight: 20,
+        paddingTop: 20, paddingBottom: 20,
+    },
+    selected: {
+        background: theme.palette.primary.main,
+        height: 3
+    },
+    notselected: {
+        height: 3
+    },
+    tooltip: {
+        color: theme.palette.common.white,
+        background: theme.palette.primary.main,
+        height: 20,
+        minWidth: 20,
+        borderRadius: 10,
+        display: 'inline-block',
+        marginLeft: 20,
+        lineHeight: '20px',
+        paddingLeft: 5,
+        paddingRight: 5,
+    }
+}));
+
 const RaceTab = ({code, n, selected, onClick} : {
     code: string,
     n:number,
     selected: boolean,
     onClick: (race: string) => void
 }) => {
-    return <span>
-        <Button onClick={() => onClick(code)}>
-    <span>{code}</span>
-    <span>({n})</span>
-    </Button>
-    </span>
+    const classes = useStyle({});
+
+    return <div className={classes.tab}>
+        <Button onClick={() => onClick(code)} className={classes.button} title={`${n} coureurs inscrits en catégorie ${code}`}>
+            <span>{code}</span>
+            <div className={classes.tooltip}>{n}</div>
+        </Button>
+        <div className={selected ? classes.selected: classes.notselected}/>
+    </div>
 }
 
 const RaceTabs = ({tabs, value, onChange}: IRaceTabs) => (
