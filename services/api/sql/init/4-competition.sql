1e7f49da791e414b69254291db25000fa9bb149c
jeromeruby <ruby.jerome@hotmail.fr>
2019-10-11 15:33:36 +0200

    Add data in database to test stats page

--- services/api/sql/init/4-competition.sql
+++ services/api/sql/init/4-competition.sql
@@ -1,2 +1,17 @@
 INSERT INTO public.competition (id, name, "categories", "eventDate", fede, "clubId", "zipCode", "info", observations, pricing, "races") VALUES (5, 'Grand Prix de Castanet', '1ere,2eme,3eme,4eme,5eme,Feminines', '2019-09-23 12:38:23.616000', 'FSGT', 2, '31320', 'Circuit plat', 'Epreuve ouverte aux catégories de valeurs renseignées ci-dessus, disputée sous les règlements cyclistes
 ', '{"tarifName":"FSGT","tarif":10}', '1/2/3,4/5');
+
+INSERT INTO public.competition (id, name, "categories", "eventDate", fede, "clubId", "zipCode", "info", observations, pricing, "races") VALUES (6, 'Grand Prix de Castanet', '1ere,2eme,3eme,4eme,5eme,Feminines', '2018-09-23 12:38:23.616000', 'FSGT', 2, '31320', 'Circuit plat', 'Epreuve ouverte aux catégories de valeurs renseignées ci-dessus, disputée sous les règlements cyclistes
+', '{"tarifName":"FSGT","tarif":10}', '1/2/3,4/5');
+
+INSERT INTO public.competition (id, name, "categories", "eventDate", fede, "clubId", "zipCode", "info", observations, pricing, "races") VALUES (7, 'Grand Prix de Mervilla', '1ere,2eme,3eme,4eme,5eme,Feminines', '2019-09-23 12:38:23.616000', 'FSGT', 2, '31320', 'Circuit plat', 'Epreuve ouverte aux catégories de valeurs renseignées ci-dessus, disputée sous les règlements cyclistes
+', '{"tarifName":"FSGT","tarif":10}', '1/2/3,4/5');
+
+INSERT INTO public.competition (id, name, "categories", "eventDate", fede, "clubId", "zipCode", "info", observations, pricing, "races") VALUES (8, 'Grand Prix de Mervilla', '1ere,2eme,3eme,4eme,5eme,Feminines', '2018-09-23 12:38:23.616000', 'FSGT', 2, '31320', 'Circuit plat', 'Epreuve ouverte aux catégories de valeurs renseignées ci-dessus, disputée sous les règlements cyclistes
+', '{"tarifName":"FSGT","tarif":10}', '1/2/3,4/5');
+
+INSERT INTO public.competition (id, name, "categories", "eventDate", fede, "clubId", "zipCode", "info", observations, pricing, "races") VALUES (9, 'Grand Prix de Rieumes', '1ere,2eme,3eme,4eme,5eme,Feminines', '2019-09-23 12:38:23.616000', 'UFOLEP', 2, '31320', 'Circuit plat', 'Epreuve ouverte aux catégories de valeurs renseignées ci-dessus, disputée sous les règlements cyclistes
+', '{"tarifName":"FSGT","tarif":10}', '1,2/3,4/5');
+
+INSERT INTO public.competition (id, name, "categories", "eventDate", fede, "clubId", "zipCode", "info", observations, pricing, "races") VALUES (10, 'Grand Prix de Rieumes', '1ere,2eme,3eme,4eme,5eme,Feminines', '2018-09-23 12:38:23.616000', 'UFOLEP', 2, '31320', 'Circuit plat', 'Epreuve ouverte aux catégories de valeurs renseignées ci-dessus, disputée sous les règlements cyclistes
+', '{"tarifName":"FSGT","tarif":10}', '1,2/3,4/5');
