5f1fffd28faa2ee189479785981ac11a3ba18067
Sami <sami.jaber@gmail.com>
2019-10-30 23:36:21 +0100

    Build & DockerCompose for staging/production Step 1

--- services/api/src/services/users.service.ts
+++ services/api/src/services/users.service.ts
@@ -1,39 +1,45 @@
 import {Injectable} from '@nestjs/common';
 import {User} from '../entity/User';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+
+const users = [
+    {
+        id: 1,
+        firstName: 'Xavier',
+        lastName: 'Gouze',
+        password: 'azerty',
+        email: 'xavier@gouze.com',
+        phone: '044343000',
+    },
+    {
+        id: 2,
+        firstName: 'admin',
+        lastName: 'admin',
+        password: 'azerty',
+        email: 'admin@admin.com',
+        phone: '+1 (830) 402-2797',
+    },
+    {
+        id: 3,
+        firstName: 'Fields',
+        lastName: 'Payne',
+        password: '583538ea3b30af778abe5983',
+        email: 'fields.payne@undefined.us',
+        phone: '+1 (899) 573-2046',
+    },
+];
 
 @Injectable()
 export class UsersService {
-    private readonly users: User[];
-    constructor() {
-        this.users = [
-            {
-                id: '583538e9605c9e261eb80042',
-                firstName: 'Xavier',
-                lastName: 'Gouze',
-                password: 'azerty',
-                email: 'xavier@gouze.com',
-                phone: '044343000',
-            },
-            {
-                id: '583538ea448f69d5dfa9dab0',
-                firstName: 'admin',
-                lastName: 'admin',
-                password: 'azerty',
-                email: 'admin@admin.com',
-                phone: '+1 (830) 402-2797',
-            },
-            {
-                id: '583538eabf9ec74ea879c08a',
-                firstName: 'Fields',
-                lastName: 'Payne',
-                password: '583538ea3b30af778abe5983',
-                email: 'fields.payne@undefined.us',
-                phone: '+1 (899) 573-2046',
-            },
-        ];
-    }
+    constructor(
+        @InjectRepository(User)
+        private readonly repository: Repository<User>,
+    ) {}
 
     async findOne(username: string): Promise<User | undefined> {
-        return this.users.find(user => user.email === username);
+        const user: User = await this.repository.findOne({email: username});
+        console.log('USER =' + JSON.stringify(user));
+        return user;
     }
 }
