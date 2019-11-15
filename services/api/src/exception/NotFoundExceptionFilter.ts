6e6dabdac8b89f4d285f1a562f9c92c07c5829c4
Sami <sami.jaber@gmail.com>
2019-11-15 11:35:11 +0100

    Fix NFException

--- services/api/src/exception/NotFoundExceptionFilter.ts
+++ services/api/src/exception/NotFoundExceptionFilter.ts
@@ -11,7 +11,7 @@ export class NotFoundExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse();
-        console.log('Not Found Page called in NestJS Web Server, redirecting to index page');
-        return response.redirect(301, 'http://localhost:3000/index.html');
+        console.log('Not Found Page called in NestJS Web Server, redirecting to index page ');
+        return response.redirect(301, '/');
     }
 }
