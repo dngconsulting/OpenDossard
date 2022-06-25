0ac12399afa4bba7bac9d68186bff80b42906e88
Sami <sami.jaber@gmail.com>
2022-06-25 14:21:03 +0200

    Fix competition chooser category column

--- services/webapp/src/pages/competition/CompetitionChooser.tsx
+++ services/webapp/src/pages/competition/CompetitionChooser.tsx
@@ -1,27 +1,18 @@
-import React, { useContext, useState } from "react";
-import {
-  createStyles,
-  makeStyles,
-  Theme,
-  withStyles
-} from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import cadtheme from "../../App";
-import { apiCompetitions } from "../../util/api";
-import {
-  CompetitionEntity,
-  CompetitionEntity as Competition,
-  RaceRow
-} from "../../sdk";
-import { Tooltip } from "@material-ui/core";
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-import { toMMDDYYYY } from "../../util/date";
-import { NotificationContext } from "../../components/CadSnackbar";
-import { styles } from "../../navigation/styles";
-import { Delete, EditRounded } from "@material-ui/icons";
-import { ConfirmDialog } from "../../util";
-import { CompetitionFilterPanel } from "../../components/CompetitionFilterPanel";
+import React, { useContext, useState } from 'react';
+import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import cadtheme from '../../App';
+import { apiCompetitions } from '../../util/api';
+import { CompetitionEntity, CompetitionEntity as Competition, RaceRow } from '../../sdk';
+import { Tooltip } from '@material-ui/core';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { toMMDDYYYY } from '../../util/date';
+import { NotificationContext } from '../../components/CadSnackbar';
+import { styles } from '../../navigation/styles';
+import { Delete, EditRounded } from '@material-ui/icons';
+import { ConfirmDialog } from '../../util';
+import { CompetitionFilterPanel } from '../../components/CompetitionFilterPanel';
 
 interface ICompetitionChooserProps {
   classes?: any;
@@ -35,16 +26,16 @@ interface ICompetitionChooserProps {
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: "100%",
-      height: "100%",
-      padding: "10px"
+      width: '100%',
+      height: '100%',
+      padding: '10px'
     },
     titre: {
-      padding: "10px",
-      fontWeight: "bold"
+      padding: '10px',
+      fontWeight: 'bold'
     },
     nocomp: {
-      padding: "20px"
+      padding: '20px'
     }
   })
 );
@@ -62,17 +53,15 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
 
   const goToPage = (competitionId: number, resultsPage?: string) => {
     props.history.push({
-      pathname: `/competition/${competitionId}/${
-        resultsPage ? resultsPage : "engagement"
-      }`,
-      state: { title: resultsPage ? "Résultats" : "Engagements" }
+      pathname: `/competition/${competitionId}/${resultsPage ? resultsPage : 'engagement'}`,
+      state: { title: resultsPage ? 'Résultats' : 'Engagements' }
     });
   };
 
   const goToPageUpdate = (competitionId: number) => {
     props.history.push({
       pathname: `/competition/update/${competitionId}`,
-      state: { title: "Modification épreuve" }
+      state: { title: 'Modification épreuve' }
     });
   };
 
@@ -81,15 +70,14 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
   };
 
   const displayEngagement = (competition: Competition) => {
-    const nbEngages = raceRows.filter(rr => rr.competitionId === competition.id)
-      .length;
+    const nbEngages = raceRows.filter(rr => rr.competitionId === competition.id).length;
     return nbEngages > 0 ? (
       <Tooltip key="2" title="Editer les engagements">
         <a
           href="#"
-          onClick={() => goToPage(competition.id, "engagement")}
+          onClick={() => goToPage(competition.id, 'engagement')}
           color="primary"
-          style={{ marginRight: "0px" }}
+          style={{ marginRight: '0px' }}
         >
           {nbEngages} Engagé(s)
         </a>
@@ -101,17 +89,15 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
 
   const displayClassement = (competition: Competition) => {
     const nbClasses = raceRows.filter(
-      rr =>
-        rr.competitionId === competition.id &&
-        (rr.comment != null || rr.rankingScratch != null)
+      rr => rr.competitionId === competition.id && (rr.comment != null || rr.rankingScratch != null)
     ).length;
     return nbClasses > 0 ? (
       <Tooltip key="2" title="Editer/Visualiser les classements">
         <a
           href="#"
-          onClick={() => goToPage(competition.id, "results/edit")}
+          onClick={() => goToPage(competition.id, 'results/edit')}
           color="primary"
-          style={{ marginRight: "0px" }}
+          style={{ marginRight: '0px' }}
         >
           {nbClasses} Classé(s)
         </a>
@@ -122,7 +108,7 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
   };
 
   const setSelectedCompetition = (selectedCompetition: Competition) => {
-    goToPage(selectedCompetition.id, "engagement");
+    goToPage(selectedCompetition.id, 'engagement');
   };
 
   const editIcon = (compRow: CompetitionEntity): JSX.Element => {
@@ -133,7 +119,7 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
             event.stopPropagation();
             goToPageUpdate(compRow.id);
           }}
-          fontSize={"default"}
+          fontSize={'default'}
         />
       </Tooltip>
     );
@@ -148,7 +134,7 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
             openDialog(true);
             setCompetitionToDelete(compRow);
           }}
-          fontSize={"default"}
+          fontSize={'default'}
         />
       </Tooltip>
     );
@@ -157,13 +143,13 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
   return (
     <Paper className={classes.root}>
       <ConfirmDialog
-        title={"Attention"}
+        title={'Attention'}
         question={
           "Êtes-vous sûr de vouloir supprimer DEFINITIVEMENT cette épreuve (s'il existe un coureur engagé, cette suppression sera interdite) ?"
         }
         open={open}
-        confirmMessage={"Oui je souhaite supprimer cette épreuve"}
-        cancelMessage={"Non"}
+        confirmMessage={'Oui je souhaite supprimer cette épreuve'}
+        cancelMessage={'Non'}
         handleClose={() => openDialog(false)}
         handleOk={async () => {
           openDialog(false);
@@ -175,7 +161,7 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
           } catch {
             setNotification({
               message: `L'épreuve' ${competitionToDelete.name} n'a pu être effacée`,
-              type: "error",
+              type: 'error',
               open: true
             });
           } finally {
@@ -192,8 +178,7 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
       />
 
       <div className={classes.titre}>
-        {data?.length} épreuve(s) affichée(s), cliquez sur une ligne pour
-        Engager/Classer
+        {data?.length} épreuve(s) affichée(s), cliquez sur une ligne pour Engager/Classer
       </div>
 
       <DataTable
@@ -212,51 +197,37 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
           setSelectedCompetition(data.find(d => d.id === e.data.id));
         }}
       >
-        <Column
-          header="Engagements"
-          body={displayEngagement}
-          style={{ minWidth: "2%", textAlign: "center" }}
-        />
-        <Column
-          header="Classements"
-          body={displayClassement}
-          style={{ minWidth: "5%", textAlign: "center" }}
-        />
-        <Column
-          field="eventDate"
-          header="Date"
-          sortable={true}
-          body={displayDate}
-          style={{ minWidth: "2%" }}
-        />
+        <Column header="Engagements" body={displayEngagement} style={{ minWidth: '2%', textAlign: 'center' }} />
+        <Column header="Classements" body={displayClassement} style={{ minWidth: '5%', textAlign: 'center' }} />
+        <Column field="eventDate" header="Date" sortable={true} body={displayDate} style={{ minWidth: '2%' }} />
         <Column
           field="name"
           header="Nom de l'épreuve"
           sortable={true}
           filter={true}
-          filterMatchMode={"contains"}
-          style={{ minWidth: "2%" }}
+          filterMatchMode={'contains'}
+          style={{ minWidth: '2%' }}
         />
         <Column
           field="zipCode"
           sortable={true}
           filter={true}
-          filterMatchMode={"contains"}
+          filterMatchMode={'contains'}
           header="Lieu"
-          style={{ minWidth: "2%" }}
+          style={{ minWidth: '2%' }}
         />
         <Column
           sortable={true}
           filter={true}
-          filterMatchMode={"contains"}
+          filterMatchMode={'contains'}
           field="club.longName"
           header="Club"
-          style={{ minWidth: "5%" }}
+          style={{ minWidth: '20%' }}
         />
         <Column
           header="Catégories"
           body={(compRow: CompetitionEntity) => {
-            return compRow.races.toString();
+            return compRow.races.toString().substring(0, 20);
           }}
         />
         <Column
@@ -264,27 +235,25 @@ const CompetitionChooser = (props: ICompetitionChooserProps) => {
           filterHeaderStyle={{ minWidth: 100 }}
           sortable={true}
           header="Fédé."
-          style={{ textAlign: "center", minWidth: "5%" }}
+          style={{ textAlign: 'center', minWidth: '5%' }}
         />
         <Column
           body={(compRow: CompetitionEntity) => editIcon(compRow)}
-          style={{ textAlign: "center", minWidth: "2%" }}
+          style={{ textAlign: 'center', minWidth: '2%' }}
         />
         <Column
           header="Type"
           sortable={true}
           filterHeaderStyle={{ minWidth: 100 }}
-          field={"competitionType"}
-          style={{ minWidth: "2%", textAlign: "center" }}
+          field={'competitionType'}
+          style={{ minWidth: '2%', textAlign: 'center' }}
         />
         <Column
           body={(compRow: CompetitionEntity) => deleteIcon(compRow)}
-          style={{ textAlign: "center", minWidth: "2%" }}
+          style={{ textAlign: 'center', minWidth: '2%' }}
         />
       </DataTable>
     </Paper>
   );
 };
-export default withStyles(styles as any, { withTheme: true })(
-  CompetitionChooser as any
-);
+export default withStyles(styles as any, { withTheme: true })(CompetitionChooser as any);
