bf2dff2655649d5b99ed05f495f418e3c871acd8
Sami JABER <sami.jaber@gmail.com>
2024-01-06 22:02:21 +0100

    Typo fix

--- services/webapp/src/util/date.ts
+++ services/webapp/src/util/date.ts
@@ -1,7 +1,6 @@
 import moment from 'moment';
 
 export const toMMDDYYYY = (thedate: Date): string => {
-  console.log('the date ' + JSON.stringify(thedate));
   return moment(thedate).format('DD/MM/YYYY');
 };
 
