7e712aec87f1beb4e01ca6d0bea9646cf6deff7e
Sami JABER <sami.jaber-ext@ratpsmartsystems.com>
2025-10-22 10:38:12 +0200

    fix baremes

--- services/api/src/services/baremeFSGT31.ts
+++ services/api/src/services/baremeFSGT31.ts
@@ -1,56 +1,44 @@
 export const baremeByCateFSGT31 = [
   {
-    catev: '1',
+    catev: "1",
     ptsParticipation: 50,
     ptsBareme: (ranking: number): number =>
       ranking <= 10
-        ? [400, 370, 340, 310, 290, 270, 250, 240, 230, 220][ranking - 1]
+        ? [400, 350, 300, 250, 200, 150, 140, 130, 120, 110][ranking - 1]
         : 0,
   },
   {
-    catev: '2',
+    catev: "2",
     ptsParticipation: 40,
     ptsBareme: (ranking: number): number =>
       ranking <= 10
-        ? [300, 280, 260, 240, 210, 200, 190, 170, 160, 150][ranking - 1]
+        ? [300, 260, 220, 180, 150, 120, 100, 80, 60, 40][ranking - 1]
         : 0,
   },
   {
-    catev: '3',
+    catev: "3",
     ptsParticipation: 30,
     ptsBareme: (ranking: number): number =>
       ranking <= 10
-        ? [160, 140, 120, 110, 100, 90, 80, 70, 60, 50][ranking - 1]
+        ? [160, 140, 120, 100, 85, 70, 55, 45, 35, 30][ranking - 1]
         : 0,
   },
   {
-    catev: '4',
+    catev: "4",
     ptsParticipation: 20,
     ptsBareme: (ranking: number): number =>
-      ranking <= 10 ? [80, 70, 60, 55, 50, 45, 40, 35, 30, 25][ranking - 1] : 0,
+      ranking <= 10 ? [80, 70, 60, 50, 45, 40, 35, 30, 25, 20][ranking - 1] : 0,
   },
   {
-    catev: '5',
+    catev: "5",
     ptsParticipation: 10,
     ptsBareme: (ranking: number): number =>
       ranking <= 10 ? [40, 35, 30, 25, 20, 18, 16, 14, 12, 10][ranking - 1] : 0,
   },
   {
-    catev: '6',
+    catev: "6",
     ptsParticipation: 5,
     ptsBareme: (ranking: number): number =>
       ranking <= 10 ? [20, 15, 12, 10, 8, 6, 4, 3, 2, 1][ranking - 1] : 0,
-  },
-  {
-    catev: 'C',
-    ptsParticipation: 10,
-    ptsBareme: (ranking: number): number =>
-      ranking <= 10 ? [40, 35, 30, 25, 20, 18, 16, 14, 12, 10][ranking - 1] : 0,
-  },
-  {
-    catev: 'M',
-    ptsParticipation: 10,
-    ptsBareme: (ranking: number): number =>
-      ranking <= 10 ? [40, 35, 30, 25, 20, 18, 16, 14, 12, 10][ranking - 1] : 0,
   },
 ];
--- services/api/src/services/baremeFSGT31CX.ts
+++ services/api/src/services/baremeFSGT31CX.ts
@@ -4,7 +4,7 @@ export const baremeByCateFSGT31CX = [
     ptsParticipation: 50,
     ptsBareme: (ranking: number): number =>
       ranking <= 10
-        ? [400, 370, 340, 310, 290, 270, 250, 240, 230, 220][ranking - 1]
+        ? [400, 350, 300, 250, 200, 150, 140, 130, 120, 110][ranking - 1]
         : 0,
   },
   {
@@ -12,7 +12,7 @@ export const baremeByCateFSGT31CX = [
     ptsParticipation: 40,
     ptsBareme: (ranking: number): number =>
       ranking <= 10
-        ? [300, 280, 260, 240, 210, 200, 190, 170, 160, 150][ranking - 1]
+        ? [300, 260, 220, 180, 150, 120, 100, 80, 60, 40][ranking - 1]
         : 0,
   },
   {
@@ -20,14 +20,14 @@ export const baremeByCateFSGT31CX = [
     ptsParticipation: 30,
     ptsBareme: (ranking: number): number =>
       ranking <= 10
-        ? [160, 140, 120, 110, 100, 90, 80, 70, 60, 50][ranking - 1]
+        ? [160, 140, 120, 100, 85, 70, 55, 45, 35, 30][ranking - 1]
         : 0,
   },
   {
     catev: "4",
     ptsParticipation: 20,
     ptsBareme: (ranking: number): number =>
-      ranking <= 10 ? [80, 70, 60, 55, 50, 45, 40, 35, 30, 25][ranking - 1] : 0,
+      ranking <= 10 ? [80, 70, 60, 50, 45, 40, 35, 30, 25, 20][ranking - 1] : 0,
   },
   {
     catev: "5",
@@ -40,18 +40,6 @@ export const baremeByCateFSGT31CX = [
     ptsParticipation: 5,
     ptsBareme: (ranking: number): number =>
       ranking <= 10 ? [20, 15, 12, 10, 8, 6, 4, 3, 2, 1][ranking - 1] : 0,
-  },
-  {
-    catev: "C",
-    ptsParticipation: 10,
-    ptsBareme: (ranking: number): number =>
-      ranking <= 10 ? [40, 35, 30, 25, 20, 18, 16, 14, 12, 10][ranking - 1] : 0,
-  },
-  {
-    catev: "M",
-    ptsParticipation: 10,
-    ptsBareme: (ranking: number): number =>
-      ranking <= 10 ? [40, 35, 30, 25, 20, 18, 16, 14, 12, 10][ranking - 1] : 0,
   },
   {
     catev: "DAMES",
