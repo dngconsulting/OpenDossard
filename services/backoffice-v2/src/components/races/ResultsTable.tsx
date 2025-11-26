97f6c61f089e1d9183334c8bf0d41c463630d056
Guillaume Houis <guillaume.houis@gmail.com>
2025-11-26 09:43:48 +0100

    feat(components): create ResultsTable component

---
+++ services/backoffice-v2/src/components/races/ResultsTable.tsx
@@ -0,0 +1,53 @@
+import { DataTable } from '@/components/ui/data-table';
+import type { RaceResult } from '@/types/races';
+import type { ColumnDef } from '@tanstack/react-table';
+
+const columns: ColumnDef<RaceResult>[] = [
+  {
+    accessorKey: 'bibNumber',
+    header: 'Dossard',
+  },
+  {
+    accessorKey: 'rank',
+    header: 'Rang',
+  },
+  {
+    accessorKey: 'chrono',
+    header: 'Chrono',
+  },
+  {
+    accessorKey: 'name',
+    header: 'Nom',
+  },
+  {
+    accessorKey: 'club',
+    header: 'Club',
+  },
+  {
+    accessorKey: 'gender',
+    header: 'Sexe',
+  },
+  {
+    accessorKey: 'dept',
+    header: 'Dept',
+  },
+  {
+    accessorKey: 'category',
+    header: 'Catégorie',
+  },
+];
+
+type Props = {
+  results: RaceResult[];
+  isLoading?: boolean;
+};
+
+export const ResultsTable = ({ results, isLoading }: Props) => {
+  return (
+    <DataTable
+      columns={columns}
+      data={results}
+      isLoading={isLoading}
+    />
+  );
+};
